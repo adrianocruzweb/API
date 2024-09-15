b6c4578d2542178afbb178328bfa001a6be34c82
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-09-15 11:32:45 -0300

    insert, select by id, and update done

--- Controller/ContatoController.cs
+++ Controller/ContatoController.cs
@@ -2,11 +2,58 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Context;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace API.Controller
 {
-    public class ContatoController
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
     {
+        private readonly AgendaContext _context;
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
         
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato =_context.Contatos.Find(id);
+
+            if(contato == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contato);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null)
+                return NotFound();
+
+            contatoBanco.Name = contato.Name;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
     }
 }
